aaab7cb5fccb8483ae0b7daff0c430aae4588ea5
escyezi <escyezi@foxmail.com>
2020-04-30 20:54:03 +0800

    create new constants and annotations

--- src/main/java/one/yezii/peanut/core/constant/ClassName.java
+++
@@ -1,7 +0,0 @@
-package one.yezii.peanut.core.constant;
-
-public interface ClassName {
-    String componentAnnotation = "one.yezii.peanut.core.annotation.Component";
-    String autowiredAnnotation = "one.yezii.peanut.core.annotation.Autowired";
-    String runnerInterface = "one.yezii.peanut.core.facade.PeanutRunner";
-}
