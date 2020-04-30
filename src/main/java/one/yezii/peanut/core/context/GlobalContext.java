8947415e0deb06c649e0f3fc40a7200014c74d14
escyezi <escyezi@foxmail.com>
2020-04-30 20:54:59 +0800

    add routerMap context

--- src/main/java/one/yezii/peanut/core/context/GlobalContext.java
+++ src/main/java/one/yezii/peanut/core/context/GlobalContext.java
@@ -2,10 +2,12 @@ package one.yezii.peanut.core.context;
 
 import one.yezii.peanut.core.facade.PeanutRunner;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GlobalContext {
     public final static Map<String, Object> beans = new HashMap<>();
     public final static Map<String, PeanutRunner> runners = new HashMap<>();
+    public final static Map<String, Method> routeMap = new HashMap<>();
 }
