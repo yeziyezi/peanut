0b12091cc75c1c7e1e1a1725730f2cf7c1d54b91
escyezi <escyezi@foxmail.com>
2020-04-30 20:55:55 +0800

    rename new constant

--- src/main/java/one/yezii/peanut/core/ioc/scan/consumer/PrintAllComponentScanResultConsumer.java
+++ src/main/java/one/yezii/peanut/core/ioc/scan/consumer/PrintAllComponentScanResultConsumer.java
@@ -1,12 +1,12 @@
 package one.yezii.peanut.core.ioc.scan.consumer;
 
 import io.github.classgraph.ScanResult;
-import one.yezii.peanut.core.constant.ClassName;
+import one.yezii.peanut.core.constant.AnnotationName;
 import one.yezii.peanut.core.ioc.scan.ScanResultConsumer;
 
 public class PrintAllComponentScanResultConsumer implements ScanResultConsumer {
     @Override
     public void consume(ScanResult scanResult) {
-        scanResult.getClassesWithAnnotation(ClassName.componentAnnotation).getNames().forEach(System.out::println);
+        scanResult.getClassesWithAnnotation(AnnotationName.Component).getNames().forEach(System.out::println);
     }
 }
