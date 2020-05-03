7af06f4c0ade22a0a237708b5ca6ae7fa7199db6
escyezi <escyezi@foxmail.com>
2020-05-03 21:57:15 +0800

    Add http server

---
+++ src/main/java/one/yezii/peanut/core/server/HttpServer.java
@@ -0,0 +1,41 @@
+package one.yezii.peanut.core.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpContentCompressor;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class HttpServer {
+    private int port;
+
+    public HttpServer listen(int port) {
+        this.port = port;
+        return this;
+    }
+
+    public void start() {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .localAddress(port)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast("codec", new HttpServerCodec())
+                                .addLast("aggregator", new HttpObjectAggregator(512 * 1024))
+                                .addLast("requestHandler", new RequestHandler())
+                                .addLast("compressor", new HttpContentCompressor());
+                        ch.pipeline().addLast(new RequestHandler());
+                    }
+                });
+        try {
+            serverBootstrap.bind().sync().channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/one/yezii/peanut/core/server/RequestHandler.java
@@ -0,0 +1,8 @@
+package one.yezii.peanut.core.server;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+@ChannelHandler.Sharable
+public class RequestHandler extends ChannelInboundHandlerAdapter {
+}
