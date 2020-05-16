6267334247b112b5248b646af279497d1633c554
escyezi <escyezi@foxmail.com>
2020-05-17 01:02:17 +0800

    rearrange code and add httpServerConfiguration

--- src/main/java/one/yezii/peanut/core/bootloader/Peanut.java
+++ src/main/java/one/yezii/peanut/core/bootloader/Peanut.java
@@ -1,6 +1,7 @@
 package one.yezii.peanut.core.bootloader;
 
 import one.yezii.peanut.core.annotation.PeanutBoot;
+import one.yezii.peanut.core.configuration.HttpServerConfiguration;
 import one.yezii.peanut.core.context.GlobalContext;
 import one.yezii.peanut.core.http.HttpServer;
 import one.yezii.peanut.core.ioc.BeanManager;
@@ -10,29 +11,35 @@ import java.util.logging.Logger;
 
 public class Peanut {
     private static Logger logger = Logger.getLogger(Peanut.class.toGenericString());
-    private boolean enableServer = true;
-    private int port = 8080;
 
-    public static <T> Peanut run(Class<T> bootClass) {
-        Peanut peanut = new Peanut();
+    public static <T> void run(Class<T> bootClass) {
         try {
-            peanut.getClasses(bootClass);
+            new Peanut().boot(bootClass);
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(-1);
         }
-        GlobalContext.runners.forEach((k, v) -> v.run());
-        if (peanut.enableServer) {
-            new HttpServer().listen(peanut.port).start();
-        }
-        logger.info("Peanut Application started.");
-        return peanut;
     }
-    public <T> void getClasses(Class<T> bootClass) {
+
+    private <T> void boot(Class<T> bootClass) throws Exception {
+        getClasses(bootClass);
+        GlobalContext.runners.forEach((k, v) -> v.run());
+        startHttpServer();
+        logger.info("Peanut Application started.");
+    }
+
+    private <T> void getClasses(Class<T> bootClass) {
         if (!bootClass.isAnnotationPresent(PeanutBoot.class)) {
             logger.log(Level.SEVERE, "boot class '" + bootClass.getName() + "' without @PeanutBoot annotation");
             System.exit(-1);
         }
         new BeanManager().initBeans(bootClass.getPackageName());
+    }
+
+    private void startHttpServer() throws InterruptedException {
+        HttpServerConfiguration configuration = new HttpServerConfiguration();
+        if (configuration.enableServer) {
+            new HttpServer().listen(configuration.port).start();
+        }
     }
 }
