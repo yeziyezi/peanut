690d1769abf3591536b36cbfbec6a12c48016f0d
escyezi <escyezi@foxmail.com>
2020-05-17 00:58:02 +0800

    update ConfigurationLoader.java

--- src/main/java/one/yezii/peanut/core/configuration/ConfigurationLoader.java
+++ src/main/java/one/yezii/peanut/core/configuration/ConfigurationLoader.java
@@ -1,10 +1,28 @@
 package one.yezii.peanut.core.configuration;
 
-import one.yezii.peanut.core.context.GlobalContext;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class ConfigurationLoader {
-    public void load() {
-        //todo
-        GlobalContext.configMap.put("", "");
+    public final static Map<String, String> configs;
+
+    static {
+        //get default properties from default.properties
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("default");
+        configs = resourceBundle.keySet().stream()
+                .collect(Collectors.toMap(k -> k, resourceBundle::getString));
+    }
+
+    public static String getString(String key) {
+        return configs.get(key);
+    }
+
+    public static int getInt(String key) {
+        return Integer.parseInt(configs.get(key));
+    }
+
+    public static boolean getBool(String key) {
+        return Boolean.parseBoolean(configs.get(key));
     }
 }
