288f54bf87bdd0a3c72d37bddc421a3e30ad24ce
escyezi <escyezi@foxmail.com>
2020-05-17 21:08:51 +0800

    add static import getBool

--- src/main/java/one/yezii/peanut/core/configuration/HttpServerConfiguration.java
+++ src/main/java/one/yezii/peanut/core/configuration/HttpServerConfiguration.java
@@ -1,8 +1,9 @@
 package one.yezii.peanut.core.configuration;
 
+import static one.yezii.peanut.core.configuration.PropertiesLoader.getBool;
 import static one.yezii.peanut.core.configuration.PropertiesLoader.getInt;
 
 public class HttpServerConfiguration {
     public int port = getInt("server.port");
-    public boolean enableServer = PropertiesLoader.getBool("server.enabled");
+    public boolean enableServer = getBool("server.enabled");
 }
