3a6640edb9ee0e10b9689644fdfa7ad57a9dadac
escyezi <escyezi@foxmail.com>
2020-06-08 10:11:22 +0800

    simplify code

--- src/main/java/one/yezii/peanut/core/ioc/BeanDependencies.java
+++ src/main/java/one/yezii/peanut/core/ioc/BeanDependencies.java
@@ -20,15 +20,7 @@ public class BeanDependencies {
                 .getClassesWithAnnotation(Component.class.getName())
                 .stream()
                 .filter(classInfo -> !classInfo.isAnnotation())
-                .collect(Collectors.toMap(ClassInfo::getName, classInfo -> {
-                    List<String> dependencies = classInfo.getFieldInfo()
-                            .filter(fieldInfo -> fieldInfo.hasAnnotation(Autowired.class.getName()))
-                            .stream().map(fieldInfo -> fieldInfo.loadClassAndGetField().getType().getName())
-                            .collect(Collectors.toList());
-                    return BeanContainer.ofName(classInfo.getName()).addDependencies(dependencies)
-                            .setBeanClass(classInfo.loadClass()).addInterfaces(classInfo.getInterfaces().getNames())
-                            .addAnnotations(classInfo.getAnnotations().getNames());
-                }));
+                .collect(Collectors.toMap(ClassInfo::getName, this::getBeanContainerOfClassInfo));
     }
 
     public Map<String, Object> getBeans() {
@@ -51,5 +43,20 @@ public class BeanDependencies {
 
     public Map<String, BeanContainer> getBeanMap() {
         return beanMap;
+    }
+
+    private List<String> getDependenciesOfClassInfo(ClassInfo classInfo) {
+        return classInfo.getFieldInfo()
+                .filter(fieldInfo -> fieldInfo.hasAnnotation(Autowired.class.getName()))
+                .stream().map(fieldInfo -> fieldInfo.loadClassAndGetField().getType().getName())
+                .collect(Collectors.toList());
+    }
+
+    private BeanContainer getBeanContainerOfClassInfo(ClassInfo classInfo) {
+        return BeanContainer.ofName(classInfo.getName())
+                .addDependencies(getDependenciesOfClassInfo(classInfo))
+                .setBeanClass(classInfo.loadClass())
+                .addInterfaces(classInfo.getInterfaces().getNames())
+                .addAnnotations(classInfo.getAnnotations().getNames());
     }
 }
