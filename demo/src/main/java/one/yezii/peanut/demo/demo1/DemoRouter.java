25c4fc5879da32a2574d009a4aff72e4fee85e45
escyezi <escyezi@foxmail.com>
2020-06-22 23:16:34 +0800

    update demo code

--- demo/src/main/java/one/yezii/peanut/demo/demo1/DemoRouter.java
+++ demo/src/main/java/one/yezii/peanut/demo/demo1/DemoRouter.java
@@ -1,10 +1,12 @@
 package one.yezii.peanut.demo.demo1;
 
 import one.yezii.peanut.core.annotation.Autowired;
+import one.yezii.peanut.core.annotation.Json;
 import one.yezii.peanut.core.annotation.Route;
 import one.yezii.peanut.core.annotation.Router;
+import one.yezii.peanut.core.util.CommonMap;
 
-@Router("/demo")
+@Router("demo")
 public class DemoRouter {
     @Autowired
     private String world;
@@ -12,5 +14,11 @@ public class DemoRouter {
     @Route
     public String hello() {
         return "hello, " + world;
+    }
+
+    @Route("json")
+    @Json
+    public CommonMap json(@Json CommonMap commonMap) {
+        return commonMap;
     }
 }
